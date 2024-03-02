6b83aa823cefca7a7bd8dd020d3273291c234581
MudassirDev <mughalmudassir966@gmail.com>
2024-03-02 11:24:15 +0500

    footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (<>
         <footer className='text-center bg-black p-5 text-white mt-10'>
-            <p>This site is made by <a className='text-[#f23838] font-[500]' href="#">Mudassir</a></p>
+            <p>This site is made by <a className='text-[#f23838] font-[500]' href="https://github.com/MudassirDev" target='_blank'>Mudassir Bilal</a></p>
         </footer>
     </>)
 }
